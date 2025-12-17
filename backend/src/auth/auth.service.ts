373102d111c7bc2996ff5a7fb324f52ba555bcda
Rafa <87162567+Rafasta236@users.noreply.github.com>
2025-12-17 00:24:42 +0000

    w

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -2,6 +2,8 @@ import { UnauthorizedException, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 import { UsersService } from '../users/users.service';
+import { ConflictException } from '@nestjs/common';
+import { Role } from '@prisma/client';
 
 @Injectable()
 export class AuthService {
@@ -39,5 +41,48 @@ export class AuthService {
   async logout(userId: string) {
     await this.usersService.setRefreshTokenHash(userId, null);
     return { ok: true };
+  }
+
+  async register(name: string, email: string, password: string) {
+  const existing = await this.usersService.findByEmail(email);
+  if (existing) {
+    throw new ConflictException('Email já registado');
+  }
+
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  const user = await this.usersService.create({
+    name,
+    email,
+    passwordHash,
+    role: Role.VIEWER,
+  });
+
+  const payload = {
+    sub: user.id,
+    name: user.name,
+    email: user.email,
+    role: user.role,
+  };
+
+  const access_token = await this.jwt.signAsync(payload);
+  const refresh_token = await this.jwt.signAsync(
+    { sub: user.id },
+    { expiresIn: '7d' },
+  );
+
+  const refreshHash = await bcrypt.hash(refresh_token, 10);
+  await this.usersService.setRefreshTokenHash(user.id, refreshHash);
+
+  return {
+    access_token,
+    refresh_token,
+    user: {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+    },
+    };
   }
 }
